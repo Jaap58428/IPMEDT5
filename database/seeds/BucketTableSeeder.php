dcf24fd90ea249e81a890ecfadb417a8e4f9fc9e
Jaap Kanbier <jaapkanbier@hotmail.com>
2018-03-09 17:15:49 +0100

    changed datetime format in seeder

--- database/seeds/BucketTableSeeder.php
+++ database/seeds/BucketTableSeeder.php
@@ -12,9 +12,9 @@ class BucketTableSeeder extends Seeder
     public function run()
     {
       $bucket_list = [
-        ['latitude' => 52.208088, 'longitude' => 4.395012, 'last_full' => 05/03/2018 12:03, 'last_empty' => 05/03/2018 12:24, 'emptied_by_user_id' => 1],
-        ['latitude' => 52.206641, 'longitude' => 4.393720, 'last_full' => 06/03/2018 11:03, 'last_empty' => 05/03/2018 8:10, 'emptied_by_user_id' => 1],
-        ['latitude' => 52.199438, 'longitude' => 4.387083, 'last_full' => 09/03/2018 14:03, 'last_empty' => 09/03/2018 19:24, 'emptied_by_user_id' => 2],
+        ['latitude' => 52.208088, 'longitude' => 4.395012, 'last_full' => date(05-03-2018 12:03:00), 'last_empty' => date(04-03-2018 12:03:00), 'emptied_by_user_id' => 1],
+        ['latitude' => 52.206641, 'longitude' => 4.393720, 'last_full' => date(02-03-2018 12:03:00), 'last_empty' => date(03-03-2018 12:03:00), 'emptied_by_user_id' => 1],
+        ['latitude' => 52.199438, 'longitude' => 4.387083, 'last_full' => date(07-03-2018 12:03:00), 'last_empty' => date(07-03-2018 13:03:00), 'emptied_by_user_id' => 2],
       ];
 
       foreach ($bucket_list as $bucket) {
