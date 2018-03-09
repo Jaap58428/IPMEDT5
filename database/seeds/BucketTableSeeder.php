063f358bed1bd815a42a45aa7526de98ee33e9c6
Jaap Kanbier <jaapkanbier@hotmail.com>
2018-03-09 17:18:44 +0100

    set seeder to now

--- database/seeds/BucketTableSeeder.php
+++ database/seeds/BucketTableSeeder.php
@@ -12,9 +12,9 @@ class BucketTableSeeder extends Seeder
     public function run()
     {
       $bucket_list = [
-        ['latitude' => 52.208088, 'longitude' => 4.395012, 'last_full' => date(05-03-2018 12:03:00), 'last_empty' => date(04-03-2018 12:03:00), 'emptied_by_user_id' => 1],
-        ['latitude' => 52.206641, 'longitude' => 4.393720, 'last_full' => date(02-03-2018 12:03:00), 'last_empty' => date(03-03-2018 12:03:00), 'emptied_by_user_id' => 1],
-        ['latitude' => 52.199438, 'longitude' => 4.387083, 'last_full' => date(07-03-2018 12:03:00), 'last_empty' => date(07-03-2018 13:03:00), 'emptied_by_user_id' => 2],
+        ['latitude' => 52.208088, 'longitude' => 4.395012, 'last_full' => Carbon::now()->format('d-m-Y H:i:s'), 'last_empty' => Carbon::now()->format('d-m-Y H:i:s'), 'emptied_by_user_id' => 1],
+        ['latitude' => 52.206641, 'longitude' => 4.393720, 'last_full' => Carbon::now()->format('d-m-Y H:i:s'), 'last_empty' => Carbon::now()->format('d-m-Y H:i:s'), 'emptied_by_user_id' => 1],
+        ['latitude' => 52.199438, 'longitude' => 4.387083, 'last_full' => Carbon::now()->format('d-m-Y H:i:s'), 'last_empty' => Carbon::now()->format('d-m-Y H:i:s'), 'emptied_by_user_id' => 2],
       ];
 
       foreach ($bucket_list as $bucket) {
